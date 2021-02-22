0e40f3e4dcf608b61092e142efc9c8a5fb834725
LAPTOP-ILL\IL <dlwodlf000@naver.com>
2021-02-22 17:04:42 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Header from "../components/Header"
+
+const Index = () => {
+    return (
+        <div>
+            <Header/>
+            <p>index page</p>
+        </div>
+    );
+}
+
+export default Index;
---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div>
+            <p>Hello, my name is Lee, i use next.js</p>
+        </div>
+    );
+}
